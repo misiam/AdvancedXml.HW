9a1838e1ec27e69c110c71c5f99806f8d354174e
Mikhail_Marykin <Mikhail_Marykin@epam.com>
2017-04-07 16:18:09 +0300

    Задание 1. Проверка файла обмена Для передачи данных о поступивших в фонды книгах используется обмен XML-файлами той же структуры, что и само хранилище (см. books.xml). Библиотечная программа умеет читать эти файлы и добавлять нужную информацию в хранилище. К сожалению, если в транспортном файле нарушена структура, то загрузка завершается ошибкой, с невнятным сообщением и без какой-либо диагностики, где именно нарушена структура. Ваша задача: •	Разработать XML Schema для транспортного файла. При этом чтобы делались такие проверки: o	Тэг isbn – опциональный, но если присутствует, должен содержать корректный ISBN номер o	Тэг genre содержит значения из заранее предопределенного списка (пусть это будут те жанры, которые присутствуют в books.xml) o	Тэги publish_date и registration_date содержат даты в формате yyyy-mm-dd. o	Атрибут id – является идентификатором, уникальным в рамках документа •	Написать утилиту проверки, которая используя разработанную схему, проверяет входной файл и выдает места обнаружения ошибок (например, можно выдавать id тех книг, в которых найдены ошибки, или строку и позицию тэга с ошибкой, ...).

---
+++ AdvancedXml.HW.CheckInputXml/AdvancedXml.HW.CheckInputXml/InputFileValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace AdvancedXml.HW.CheckInputXml
+{
+    internal class InputFileValidator
+    {
+        private readonly XmlReaderSettings _settings;
+
+        private IList<string> _fileErrors = new List<string>();
+
+        public InputFileValidator(string xsdSchema)
+        {
+            var settings = new XmlReaderSettings();
+
+            settings.Schemas.Add("http://library.by/catalog", xsdSchema);
+            settings.ValidationEventHandler +=
+                delegate (object sender, ValidationEventArgs e)
+                {
+                    string errorMessage = string.Format("[line_{0}:pos_{1}] {2}", e.Exception.LineNumber, e.Exception.LinePosition, e.Message);
+                    _fileErrors.Add(errorMessage);
+                    Console.WriteLine(errorMessage);
+                };
+
+            settings.ValidationFlags = settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationType = ValidationType.Schema;
+
+            this._settings = settings;
+
+        }
+
+        public IList<string> ValidateErrors(string inputFile)
+        {
+
+            _fileErrors.Clear();
+
+            using (XmlReader reader = XmlReader.Create(inputFile, _settings))
+            {
+                while (reader.Read()) ;
+            }
+
+            return _fileErrors.ToList();
+        }
+
+    }
+}
---
+++ AdvancedXml.HW.CheckInputXml/AdvancedXml.HW.CheckInputXml/InputFileValidatorTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdvancedXml.HW.CheckInputXml
+{
+    [TestClass]
+    public class InputFileValidatorTest
+    {
+        private InputFileValidator _fileValidator;
+
+        [TestInitialize]
+        public void Init()
+        {
+            string xsdSchema = "XMLSchemaBooks.xsd"; 
+            _fileValidator = new InputFileValidator(xsdSchema);
+        }
+
+        [TestMethod]
+        public void TestValidateCorrectFile()
+        {
+            string inputFile = @".\CorrectFiles\books.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(!errors.Any());
+
+        }
+
+
+        [TestMethod]
+        public void TestValidateIncorrectFile()
+        {
+            string inputFile = @".\IncorrectFiles\books_wrong_format.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(errors.Select(x=>x.Contains("has invalid child element 'WRONG'")).Any());
+        }
+
+        [TestMethod]
+        public void TestValidateIncorrectIsbn()
+        {
+            string inputFile = @".\IncorrectFiles\books_wrong_isbn.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(errors.Select(x=>x.Contains(@"'http://library.by/catalog:isbn' - The Pattern constraint failed")).Any());
+        }
+
+        [TestMethod]
+        public void TestValidateIncorrectGenre()
+        {
+            string inputFile = @".\IncorrectFiles\books_wrong_genre.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(errors.Select(x => x.Contains(@"'http://library.by/catalog:genre' - The Enumeration constraint failed.")).Any());
+        }
+
+        [TestMethod]
+        public void TestValidateIncorrectDateFormat()
+        {
+            string inputFile = @".\IncorrectFiles\books_wrong_date_format.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(errors.Select(x => x.Contains("is not a valid Date value.")).Any());
+        }
+
+        [TestMethod]
+        public void TestValidateNotUniqId()
+        {
+            string inputFile = @".\IncorrectFiles\books_not_uniq_id.xml";
+            var errors = _fileValidator.ValidateErrors(inputFile);
+            Assert.IsTrue(errors.Select(x => x.Contains("There is a duplicate key sequence")).Any());
+        }
+
+    }
+}
